5d55e562379d1d9bcfa0fda82f1c870ea0234cb4
Lars Anderson <ishka912@gmail.com>
2013-05-07 23:46:42 -0500

    Added documentation

--- PlaceKit/PlaceKit.h
+++ PlaceKit/PlaceKit.h
@@ -33,63 +33,166 @@ extern NSString * const kPLKPlaceRandomGreyscaleImageURLString;
 
 @interface PlaceKit : NSObject
 
-/** Placeholder images
+//------–----------------
+/** Placeholder Images */
+//------–----------------
+
+/** Place a kitten image with a given size from placekitten.com
+ 
+ @param size The size in points of your kitten image
+ @param completionBlock The block that your kitten image will be returned in
  */
-//placekitten.com
-//baconmockup.com
-//placehold.it
-//lorempixel.com
 + (void)placeKittenImageWithSize:(CGSize)size
                       completion:(void(^)(UIImage *kittenImage))completionBlock;
+
+/** Place an image of bacon or other meat with a given size from baconmockup.com
+ 
+ @warning *Warning:* May motivate designers at the expense of developer happiness.
+ 
+ @param size The size in points of your meat-based image
+ @param completionBlock The block that your meat-based image will be returned in
+ */
 + (void)placeBaconImageWithSize:(CGSize)size
                      completion:(void(^)(UIImage *baconImage))completionBlock;
+
+/** Place a generic placeholder image with a given size from http://placehold.it
+ 
+ @param size The size in points of your placeholder
+ @param completionBlock The block that your placeholder image will be returned in
+ */
 + (void)placeHolderImageWithSize:(CGSize)size
                       completion:(void(^)(UIImage *placeholderImage))completionBlock;
+
+/** Place a random image in a specific category with a given size from lorempixel.com
+ 
+ @param size The size in points of your random image
+ @param category The category that you would like your image to be chosen from. A full list of valid categories can be found at http://lorempixel.com
+ @param completionBlock The block that your random image will be returned in
+ */
 + (void)placeRandomImageWithSize:(CGSize)size
                         category:(NSString *)category
                       completion:(void(^)(UIImage *randomImage))completionBlock;
+
+/** Place a random greyscale image in a specific category with a given size from lorempixel.com
+ 
+ @param size The size in points of your random greyscale image
+ @param category The category that you would like your image to be chosen from. A full list of valid categories can be found at http://lorempixel.com
+ @param completionBlock The block that your random image will be returned in
+ */
 + (void)placeRandomGreyscaleImageWithSize:(CGSize)size
                                  category:(NSString *)category
                                completion:(void(^)(UIImage *randomImage))completionBlock;
+
+/** Place a random image with a given size from lorempixel.com
+ 
+ @param size The size in points of your random image
+ @param completionBlock The block that your placeholder image will be returned in
+ */
 + (void)placeRandomImageWithSize:(CGSize)size
                       completion:(void(^)(UIImage *randomImage))completionBlock;
+
+/** Place a random greyscale image with a given size from lorempixel.com
+ 
+ @param size The size in points of your random greyscale image
+ @param completionBlock The block that your placeholder image will be returned in
+ */
 + (void)placeRandomGreyscaleImageWithSize:(CGSize)size
                                completion:(void(^)(UIImage *randomImage))completionBlock;
 
-/** Placeholder text
+//------–------------------
+/** @name Placeholder text */
+//------–------------------
+
+/** Place some random text with the given parameters from loripsum.net.
+ 
+ @param numberOfParagraphs The number of paragraphs of text that you would like.
+ @param paragraphLength The requested length of each paragraph
+ @param options A bitmask of PLKTextOptions
+ @param completionBlock The block that your random text will be returned in
  */
-//loripsum.net
 + (void)placeTextWithNumberOfParagraphs:(NSInteger)numberOfParagraphs
                                ofLength:(PLKTextParagraphLength)paragraphLength
                                 options:(PLKTextOptions)options
                              completion:(void(^)(NSString *placeText))completionBlock;
 
-/** Placeholder data
- */
-/** Random names
+//------–-------------------
+/** @name Placeholder data */
+//------–-------------------
+
+/** Place a random first name
  */
 + (NSString *)placeRandomFirstName;
+
+/** Place a random last name
+ */
 + (NSString *)placeRandomLastName;
+
+/** Place a random full name
+ */
 + (NSString *)placeRandomFullName;
 
-/** Random business name
+/** Place a random business name with a given word count
  */
 + (NSString *)placeRandomBusinessNameWithNumberOfWords:(NSUInteger)words;
 
-/** Random numbers
+//------–------------------
+/** @name Random numbers */
+//------–------------------
+
+/** Place a series of random numbers formatter like a US phone number
  */
 + (NSString *)placeRandomPhoneNumber;
+
+/** Place a random integer less than a given number
+ 
+ @param lessThan A maximum bound for the random number generator
+ */
 + (NSInteger)placeRandomIntegerLessThan:(NSInteger)lessThan;
+
+/** Place a random float less than a given number
+ 
+ @param lessThan A maximum bound for the random number generator
+ */
 + (CGFloat)placeRandomFloatLessThan:(NSInteger)lessThan;
+
+/** Place a random float within a given range
+ 
+ @param range The range in which the random float be returned
+ */
 + (CGFloat)placeRandomFloatInRange:(NSRange)range;
+
+/** Place a random float value in the range [0,1]
+ */
 + (CGFloat)placeRandomPercentage;
 
-/** Geometry
+//------–-----------
+/** @name Geometry */
+//------–-----------
+
+/** Place a random CGSize with random dimensions bounded by a given range
+ 
+ @param dimensionRange A range to bound each dimension to
  */
 + (CGSize)placeRandomSizeWithDimensionInRange:(NSRange)dimensionRange;
+
+/** Place a random CGSize with unique ranges for each dimension
+ 
+ @param xDimensionRange A range for the dimension in the x-direction (width)
+ @param yDimensionRange A range for the dimension in the y-direction (height)
+ */
 + (CGSize)placeRandomSizeWithXRange:(NSRange)xDimensionRange
                              yRange:(NSRange)yDimensionRange;
+
+/** Place a random CGRect that is fully contained within another rect.
+ 
+ @param rect A rect to bound the randomly generated rect within
+ */
 + (CGRect)placeRandomRectWithinRect:(CGRect)rect;
+
+/** Place a random CGPoint that is located within the bounds of a given rect
+ 
+ @param rect A rect to bound the randomly generated point within
+ */
 + (CGPoint)placeRandomPointWithinRect:(CGRect)rect;
 
 @end
--- PlaceKit/UIImageView+PlaceKit.h
+++ PlaceKit/UIImageView+PlaceKit.h
@@ -8,24 +8,102 @@
 
 #import <UIKit/UIKit.h>
 
-@interface UIImageView (PlaceKit)
+/** These category methods differ from their pure block-based counterparts in that they are using AFNetworking's UIImageView+AFNetworking category under the hood. This gives these category methods some small tidbits like image caching.
+ 
+ Use the explicit size category methods when doing things in unlaid-out objects like before a collection view cell has been laid out. You may know the size of the image from the layout information, but the UIImageView that you will be using doesn't yet have a size.
+ 
+ If you only need an image or don't want things the AFNetworking category provides, use the block-based image fetching methods in PlaceKit Core.
+ */
 
+@interface UIImageView (PlaceKit)
+//------–------------------
+/** @name Explicit Sizing */
+//------–------------------
+
+/** Place a kitten image with a given size from placekitten.com
+ 
+ @param size The size in points of your kitten image
+ */
 - (void)placeKittenImageWithSize:(CGSize)size;
+
+/** Place an image of bacon or other meat with a given size from baconmockup.com
+ 
+ @warning *Warning:* May motivate designers at the expense of developer happiness.
+ 
+ @param size The size in points of your meat-based image
+ */
 - (void)placeBaconImageWithSize:(CGSize)size;
+
+/** Place a generic placeholder image with a given size from http://placehold.it
+ 
+ @param size The size in points of your placeholder
+ */
 - (void)placeHolderImageWithSize:(CGSize)size;
+
+/** Place a random image in a specific category with a given size from lorempixel.com
+ 
+ @param size The size in points of your random image
+ @param category The category that you would like your image to be chosen from. A full list of valid categories can be found at http://lorempixel.com
+ */
 - (void)placeRandomImageWithSize:(CGSize)size
                         category:(NSString *)category;
+
+/** Place a random greyscale image in a specific category with a given size from lorempixel.com
+ 
+ @param size The size in points of your random greyscale image
+ @param category The category that you would like your image to be chosen from. A full list of valid categories can be found at http://lorempixel.com
+ */
 - (void)placeRandomGreyscaleImageWithSize:(CGSize)size
                                  category:(NSString *)category;
+
+/** Place a random image with a given size from lorempixel.com
+ 
+ @param size The size in points of your random image
+ */
 - (void)placeRandomImageWithSize:(CGSize)size;
+
+/** Place a random greyscale image with a given size from lorempixel.com
+ 
+ @param size The size in points of your random greyscale image
+ */
 - (void)placeRandomGreyscaleImageWithSize:(CGSize)size;
 
+//------–------------------
+/** @name Implicit Sizing */
+//-------------------------
+
+/** Place a kitten image with an inherited size from placekitten.com
+ */
 - (void)placeKittenImage;
+
+/** Place an image of bacon or other meat with an inherited size from baconmockup.com
+ 
+ @warning *Warning:* May motivate designers at the expense of developer happiness.
+ */
 - (void)placeBaconImage;
+
+/** Place a generic placeholder image with an inherited size from http://placehold.it
+ */
 - (void)placeHolderImage;
+
+/** Place a random image in a specific category with an inherited size from lorempixel.com
+ 
+ @param category The category that you would like your image to be chosen from. A full list of valid categories can be found at http://lorempixel.com
+ */
 - (void)placeRandomImageFromCategory:(NSString *)category;
+
+/** Place a random greyscale image in a specific category with an inherited size from lorempixel.com
+ 
+ @param category The category that you would like your image to be chosen from. A full list of valid categories can be found at http://lorempixel.com
+ */
 - (void)placeRandomGreyscaleImageFromCategory:(NSString *)category;
+
+/** Place a random image with an inherited size from lorempixel.com
+ */
 - (void)placeRandomImage;
+
+/** Place a random greyscale image with an inherited size from lorempixel.com
+ */
 - (void)placeRandomGreyscaleImage;
 
 @end
